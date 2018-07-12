33150036ed9f407014ea148167d391d6b54b1ffd
Sergey19940808 <alekseyserzh88@gmail.com>
2018-07-12 19:27:36 +0500

    add section at the html

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,25 @@
 
 const gulp =  require('gulp');
 const sass = require('gulp-sass');
+const babel = require('gulp-babel');
 
-gulp.task('sass', function () {
-    return gulp.src('./assets/style.scss')
+gulp.task('js', () => {
+        return gulp.src('assets/js/app.js')
+            .pipe(babel({
+                presets: ['env']
+            }))
+            .pipe(gulp.dest('public'))
+});
+
+gulp.task('sass', () => {
+    return gulp.src('./assets/scss/style.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./public'));
 });
 
-gulp.watch('./assets/style.scss', gulp.series('sass'));
+gulp.task('watch', () => {
+    gulp.watch('./assets/scss/style.scss', gulp.series('sass'));
+    gulp.watch('./assets/js/app.js', gulp.series('js'));
+});
+
+gulp.task('start', gulp.series('sass', 'js', 'watch'));
