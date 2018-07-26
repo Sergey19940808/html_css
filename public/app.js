9faa1dbdb33d0ab0a218f9a593aea38675add01d
Sergey19940808 <alekseyserzh88@gmail.com>
2018-07-26 19:47:43 +0500

    add feature styles

--- public/app.js
+++ public/app.js
@@ -1,15 +1,2 @@
-'use strict';
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    if (window.navigator.vendor === '') {
-        var listFZContent = document.querySelectorAll('.fz-18');
-        for (var i = 0; i < listFZContent.length; ++i) {
-            listFZContent[i].style.fontSize = '15px';
-        }
-
-        var listFZHeader = document.querySelectorAll('.fz-25');
-        for (var _i = 0; _i < listFZHeader.length; ++_i) {
-            listFZHeader[_i].style.fontSize = '20px';
-        }
-    }
-});
+  
